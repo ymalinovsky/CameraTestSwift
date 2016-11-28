385afddeec4b735428db4eae5596be9cdc233fa4
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-28 18:33:26 +0200

    Selecting Image

--- CameraTest/ViewController.swift
+++ CameraTest/ViewController.swift
@@ -27,12 +27,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func cameraAction(_ sender: UIBarButtonItem) {
         imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+        
         present(imagePicker, animated: true, completion: nil)
     }
+    
 
     @IBAction func photoAction(_ sender: UIBarButtonItem) {
         imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image: UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
